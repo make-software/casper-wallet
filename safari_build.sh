6f5869cb567ddb94d1a4ad0c5a40aa4838d85582
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-02-14 17:55:12 +0200

    Refactored safari build process

--- safari_build.sh
+++ safari_build.sh
@@ -1,9 +1,11 @@
 # Safari is required extension with manifest v2. So we can use Firefox's build for now
-# We should watch it and update build process according to used manifest in scenarios (package.json:scripts).
+
+npm run build:firefox
 
 xcrun safari-web-extension-converter --no-prompt --no-open ./build/firefox
 
 cd CasperLabs\ Signer
 xcodebuild -scheme CasperLabs\ Signer\ \(macOS\) build
 cd ..
+
 rm -rf CasperLabs\ Signer
