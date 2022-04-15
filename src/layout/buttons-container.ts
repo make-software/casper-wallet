b319d66aad6c7b8c4e7a717a7205795107231152
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-04-15 17:51:12 +0300

    Added `direction` prop to `ButtonsContainer` with default value `row`

--- src/layout/buttons-container.ts
+++ src/layout/buttons-container.ts
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
-export const ButtonsContainer = styled.div`
+// Default direction value is `row`
+interface Props {
+  direction?: 'row' | 'column';
+}
+
+export const ButtonsContainer = styled.div<Props>`
+  display: flex;
+  flex-direction: ${({ direction }) => (direction ? direction : 'row')};
+  justify-content: space-around;
+  gap: 20px;
+
   border-top: ${({ theme }) => `1px solid ${theme.color['gray1.5']}`};
   padding: ${({ theme }) => theme.padding[1.333]};
+
+  width: 100%;
 `;
