02fa17d530faf5ab42b14154601ca67a9a0c2a1b
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-06-21 17:35:21 +0300

    Fixed scrollbars. MacOS scrollbar appearance for all browsers

--- src/apps/import-account-with-file/index.tsx
+++ src/apps/import-account-with-file/index.tsx
@@ -3,7 +3,10 @@ import '@libs/i18n/i18n';
 import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { GlobalScrollbar } from 'mac-scrollbar';
 import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import 'mac-scrollbar/dist/mac-scrollbar.css';
 
 import { GlobalStyle, themeConfig } from '@libs/ui';
 
@@ -18,6 +21,7 @@ render(
   <Suspense fallback={null}>
     <ThemeProvider theme={themeConfig}>
       <GlobalStyle />
+      <GlobalScrollbar />
       <HashRouter>
         <Routes>
           <Route
--- src/apps/popup/index.tsx
+++ src/apps/popup/index.tsx
@@ -2,7 +2,10 @@ import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { GlobalScrollbar } from 'mac-scrollbar';
 import { ThemeProvider } from 'styled-components';
+
+import 'mac-scrollbar/dist/mac-scrollbar.css';
 
 import { GlobalStyle, themeConfig } from '@libs/ui';
 
@@ -38,6 +41,7 @@ render(
       <ThemeProvider theme={themeConfig}>
         <ReduxProvider store={store}>
           <GlobalStyle />
+          <GlobalScrollbar />
           <HashRouter>
             <App />
           </HashRouter>
