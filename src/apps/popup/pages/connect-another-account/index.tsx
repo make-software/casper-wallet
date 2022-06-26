697e6dcc5571e3a0d61a435701d0ba6fd746282c
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-06-26 15:48:56 +0300

    Added `ConnectAnotherAccount` page

--- src/apps/popup/pages/connect-another-account/index.tsx
+++ src/apps/popup/pages/connect-another-account/index.tsx
@@ -1,28 +1,133 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Trans, useTranslation } from 'react-i18next';
 import { RootState } from 'typesafe-actions';
+import styled from 'styled-components';
+
+import { useActiveTabOrigin } from '@popup/hooks/use-active-tab-origin';
 
 import { ContentContainer, HeaderTextContainer } from '@src/layout';
-import { Typography } from '@libs/ui';
-import { useActiveTabOrigin } from '@popup/hooks/use-active-tab-origin';
-import { useSelector } from 'react-redux';
-import { selectConnectedAccountsToActiveTab } from '@popup/redux/vault/selectors';
+import {
+  Button,
+  CurrentSiteFavicon,
+  Hash,
+  HashVariant,
+  List,
+  PageTile,
+  SvgIcon,
+  Typography
+} from '@libs/ui';
+
+import {
+  selectConnectedAccountsToActiveTab,
+  selectVaultActiveAccount
+} from '@popup/redux/vault/selectors';
+
+const HeaderTextContent = styled.div`
+  margin-top: 16px;
+`;
+
+const CentredFlexRow = styled.div`
+  display: flex;
+  width: 100%;
+
+  align-items: center;
+
+  gap: 18px;
+`;
+
+const ListItemContainer = styled(CentredFlexRow)`
+  padding: 14px 18px;
+`;
+
+const LeftAlignedFlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const SpaceBetweenContainer = styled(CentredFlexRow)`
+  justify-content: space-between;
+`;
 
 export function ConnectAnotherAccountPageContent() {
+  const { t } = useTranslation();
   const activeTabOrigin = useActiveTabOrigin();
 
   const connectedAccountsToActiveTab = useSelector((state: RootState) =>
     selectConnectedAccountsToActiveTab(state, activeTabOrigin)
   );
+  const activeAccount = useSelector(selectVaultActiveAccount);
+
   return (
     <ContentContainer>
       <HeaderTextContainer>
-        <Typography type="header" weight="bold">
-          Your current account is not connected
-        </Typography>
+        <CurrentSiteFavicon
+          faviconUrl={`${activeTabOrigin}/favicon.ico`}
+          hostName={activeTabOrigin.split('://')[1]}
+        />
+        <HeaderTextContent>
+          <Typography type="header" weight="bold">
+            Your current account is not connected
+          </Typography>
+        </HeaderTextContent>
       </HeaderTextContainer>
-      {connectedAccountsToActiveTab.map(account => (
-        <div>{account.name}</div>
-      ))}
+      {activeAccount && (
+        <PageTile>
+          <CentredFlexRow>
+            <SvgIcon
+              src="assets/icons/error.svg"
+              size={24}
+              color="contentYellow"
+            />
+            <SpaceBetweenContainer>
+              <LeftAlignedFlexColumn>
+                <Typography type="body" weight="regular">
+                  {activeAccount.name}
+                </Typography>
+                <Hash
+                  value={activeAccount.publicKey}
+                  variant={HashVariant.CaptionHash}
+                  truncated
+                />
+              </LeftAlignedFlexColumn>
+              <Button variant="inline" width="100">
+                <Trans t={t}>Connect</Trans>
+              </Button>
+            </SpaceBetweenContainer>
+          </CentredFlexRow>
+        </PageTile>
+      )}
+      <List
+        headerLabel={t('Switch to another account')}
+        rows={connectedAccountsToActiveTab}
+        renderRow={account => (
+          <ListItemContainer key={account.name}>
+            <SvgIcon
+              src="assets/icons/checkbox-checked.svg"
+              size={24}
+              color="contentGreen"
+            />
+
+            <SpaceBetweenContainer>
+              <LeftAlignedFlexColumn>
+                <Typography type="body" weight="regular">
+                  {account.name}
+                </Typography>
+                <Hash
+                  value={account.publicKey}
+                  variant={HashVariant.CaptionHash}
+                  truncated
+                />
+              </LeftAlignedFlexColumn>
+              <Button color="secondaryBlue" variant="inline" width="100">
+                <Trans t={t}>Switch</Trans>
+              </Button>
+            </SpaceBetweenContainer>
+          </ListItemContainer>
+        )}
+        marginLeftForItemSeparatorLine={60}
+      />
     </ContentContainer>
   );
 }
