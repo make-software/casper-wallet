4cfe2f88e018c0b3321bdd8159d43ab9532207e8
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-08-05 17:17:01 +0300

    Refactored. Added comments to `.sh` scripts

--- build_xcode_framework.sh
+++ build_xcode_framework.sh
@@ -1,1 +1,3 @@
+# Deploying the XCode Framework for further assembly of extension by Webpack XcodeBuild plugin
+# Firefox extension built on manifest V2, the same version Safari required, so we use the Firefox build as source for extension for Safari
 xcrun safari-web-extension-converter --no-prompt --no-open --project-location ./output/safari --macos-only ./output/firefox
--- safari_build.sh
+++ safari_build.sh
@@ -1,10 +1,11 @@
-# Create Framework for build
+# Deploying the XCode Framework for building of extension for Safari
+# Firefox extension built on manifest V2, the same version Safari required, so we use the Firefox build as source for extension for Safari
 xcrun safari-web-extension-converter --no-prompt --no-open --project-location ./build/xcode-framework --macos-only ./build/firefox
 
-# Build
+# Building extension for Safari by XCode Framework
 cd ./build/xcode-framework/CasperLabs\ Signer && xcodebuild -quiet -scheme CasperLabs\ Signer -derivedDataPath ../../safari/CasperLabs\ Signer
 
-# Remove Framework
+# Remove XCode Framework
 cd ../../ && rm -rf ./xcode-framework
 
 # Add link to app file in root folder
