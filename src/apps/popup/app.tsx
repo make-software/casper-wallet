fd70eb250904996ec822664692ed6d2de3af65e1
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-09-29 15:14:18 +0300

    Added form logic and validation

--- src/apps/popup/app.tsx
+++ src/apps/popup/app.tsx
@@ -39,7 +39,12 @@ import { useVaultTimeoutController } from './hooks/use-vault-timeout-controller'
 
 export function App() {
   const vaultIsLocked = useSelector(selectVaultIsLocked);
+  const vaultDoesExist = useSelector(selectVaultDoesExist);
   useVaultTimeoutController();
+
+  if (!vaultDoesExist) {
+    openOnboardingAppInTab();
+  }
 
   if (vaultIsLocked) {
     return <LockedRouter />;
