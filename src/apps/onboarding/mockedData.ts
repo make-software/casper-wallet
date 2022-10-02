c7d69f965f83ec2492fcea29c2d903cf9c31e88d
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-10-02 20:41:57 +0300

    Added mocked data

---
+++ src/apps/onboarding/mockedData.ts
@@ -0,0 +1,35 @@
+export const mockedMnemonicPhrase = [
+  { order: 1, word: 'russian' },
+  { order: 2, word: 'warship' },
+  { order: 3, word: 'go' },
+  { order: 4, word: 'f**k' },
+  { order: 5, word: 'yourself' },
+  { order: 6, word: 'crimea' },
+  { order: 7, word: 'is' },
+  { order: 8, word: 'ukraine' },
+  { order: 9, word: 'kherson' },
+  { order: 10, word: 'is' },
+  { order: 11, word: 'ukraine' },
+  { order: 12, word: 'zaporizhya' },
+  { order: 13, word: 'is' },
+  { order: 14, word: 'ukraine' },
+  { order: 15, word: 'odesa' },
+  { order: 16, word: 'is' },
+  { order: 17, word: 'ukraine' },
+  { order: 18, word: 'donbas' },
+  { order: 19, word: 'is' },
+  { order: 20, word: 'ukraine' },
+  { order: 21, word: 'peace' },
+  { order: 22, word: 'for' },
+  { order: 23, word: 'ukraine' },
+  { order: 24, word: 'forever' }
+];
+
+export const mockedWordCollection = [
+  { order: 2, word: 'warship' },
+  { order: 6, word: 'crimea' },
+  { order: 8, word: 'ukraine' },
+  { order: 18, word: 'donbas' },
+  { order: 20, word: 'ukraine' },
+  { order: 21, word: 'peace' }
+];
