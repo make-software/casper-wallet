92fe10aeb49ffee5817a0e5ba0fe5bef64952e93
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-10-04 01:43:35 +0300

    Fixed navigation

--- src/apps/onboarding/pages/wallet-created/index.tsx
+++ src/apps/onboarding/pages/wallet-created/index.tsx
@@ -4,21 +4,12 @@ import { useTranslation, Trans } from 'react-i18next';
 import { LayoutTab, TabFooterContainer } from '@libs/layout';
 import { Button } from '@libs/ui';
 
+import { closeActiveTab } from '@src/apps/onboarding/utils/closeActiveTab';
+
 import { WalletCreatedPageContent } from './content';
-import browser from 'webextension-polyfill';
 
 export function WalletCreatedPage() {
   const { t } = useTranslation();
-
-  async function closeActiveTab() {
-    const tabs = await browser.tabs.query({
-      active: true,
-      currentWindow: true
-    });
-    if (tabs.length > 0 && tabs[0].id != null) {
-      await browser.tabs.remove(tabs[0].id);
-    }
-  }
 
   return (
     <LayoutTab
