16074c0c067e7d0af4bec8df94817f0e59fcba6a
Vladimir Gachkovsky <vladimir.gachkovsky@make.services>
2022-10-07 03:50:19 +0300

    Refactored `closeActiveTab`. Catch error inside

--- src/apps/onboarding/utils/close-active-tab.ts
+++ src/apps/onboarding/utils/close-active-tab.ts
@@ -1,11 +1,15 @@
 import browser from 'webextension-polyfill';
 
 export async function closeActiveTab() {
-  const tabs = await browser.tabs.query({
-    active: true,
-    currentWindow: true
-  });
-  if (tabs.length > 0 && tabs[0].id != null) {
-    await browser.tabs.remove(tabs[0].id);
+  try {
+    const tabs = await browser.tabs.query({
+      active: true,
+      currentWindow: true
+    });
+    if (tabs.length > 0 && tabs[0].id != null) {
+      await browser.tabs.remove(tabs[0].id);
+    }
+  } catch (e) {
+    console.error(e);
   }
 }
