399e6bf9dfd2a2dc5c76f849f677888a783e37a0
Piotr Witek <piotrek.witek@gmail.com>
2022-10-24 16:02:03 +0200

    updated test description

--- src/libs/crypto/bip32.test.ts
+++ src/libs/crypto/bip32.test.ts
@@ -2,7 +2,7 @@ import { deriveKeyPair } from './bip32';
 import { EXPECTED_PUBLIC_KEY_0, SECRET_PHRASE } from './ledger-casper-fixtures';
 
 describe('bip32', () => {
-  it('should derive the same key as ledger', () => {
+  it('should derive the same key as ledger from the same mnomonic phrase', () => {
     const keyPair = deriveKeyPair(SECRET_PHRASE, 0);
     expect(keyPair.publicKey).toBe(EXPECTED_PUBLIC_KEY_0);
   });
