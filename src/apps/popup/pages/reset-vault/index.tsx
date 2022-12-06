eba5d8262553c851dd8224cd3b2e28133cf276fe
ost-ptk <ostap@make.services>
2022-12-06 12:14:04 +0200

    Fixed wallet closing on Safari browser after wallet reset

--- src/apps/popup/pages/reset-vault/index.tsx
+++ src/apps/popup/pages/reset-vault/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import browser from 'webextension-polyfill';
 
 import {
   ContentContainer,
@@ -19,8 +20,13 @@ export function ResetVaultPageContent() {
   const { t } = useTranslation();
 
   function handleResetVault() {
-    dispatchToMainStore(resetVault());
-    window.close();
+    // It's hacky for Safari browser => browser.runtime.reload();
+    // window.close() method can only be called on windows that were opened by a script using the Window.open() method.
+    // If the window was > not opened by a script, an error similar to this one appears in the console:
+    // Scripts may not close windows that were not opened by script
+    dispatchToMainStore(resetVault()).then(() => {
+      browser.runtime.reload();
+    });
   }
 
   function handleCancel() {
