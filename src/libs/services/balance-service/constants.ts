77909e4880c53eb1d80610047fb4a2e365fd88e4
Piotr Witek <piotrek.witek@gmail.com>
2022-12-09 17:11:19 +0100

    Changed balance calls to use testnet

--- src/libs/services/balance-service/constants.ts
+++ src/libs/services/balance-service/constants.ts
@@ -2,12 +2,15 @@ interface GetAccountBalanceUrl {
   publicKey: string;
 }
 
-const BASE_URL = 'https://casper-api.dev.make.services';
+// const BASE_URL_MAINNET = 'https://casper-api.dev.make.services';
+const BASE_URL_TESTNET =
+  'https://event-store-api-clarity-testnet.make.services';
 
-export const CURRENCY_RATE_URL = `${BASE_URL}/rates/1/amount`;
+export const CURRENCY_RATE_URL = `${BASE_URL_TESTNET}/rates/1/amount`;
 
 export const getAccountBalanceUrl = ({
   publicKey
-}: GetAccountBalanceUrl): string => `${BASE_URL}/accounts/${publicKey}/balance`;
+}: GetAccountBalanceUrl): string =>
+  `${BASE_URL_TESTNET}/accounts/${publicKey}/balance`;
 
 export const SECOND = 1000;
