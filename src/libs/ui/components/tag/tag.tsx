917510cb107e753845e5696f92e13631483f69ce
Ostap Piatkovskyi <44294945+ost-ptk@users.noreply.github.com>
2023-01-24 13:14:43 +0200

    Connected label misplaced in accounts list (#469)
    * Fixed place of connected and imported label
    
    * Fixed review comments
    
    * fixed dep warning
    
    Co-authored-by: ost-ptk <ostap@make.services>

--- src/libs/ui/components/tag/tag.tsx
+++ src/libs/ui/components/tag/tag.tsx
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 
 interface TagProps {
   children: ReactNode;
+  displayContext?: 'accountList';
+}
+
+interface StyleTagContainerProps {
+  displayContext?: 'accountList';
 }
 
 const StyledTag = styled.span`
@@ -11,11 +16,21 @@ const StyledTag = styled.span`
   font-weight: ${({ theme }) => theme.typography.fontWeight.semiBold};
   font-size: 0.8rem;
   line-height: 1.6rem;
-  padding: 6px;
+  padding: 0 4px;
   border-radius: ${({ theme }) => theme.borderRadius.hundred}px;
   text-transform: uppercase;
 `;
 
-export function Tag({ children }: TagProps) {
-  return <StyledTag>{children}</StyledTag>;
+const StyleTagContainer = styled.div<StyleTagContainerProps>`
+  display: inline-flex;
+  margin: ${({ displayContext }) =>
+    displayContext === 'accountList' ? '0 0 0 4px' : '0'};
+`;
+
+export function Tag({ children, displayContext }: TagProps) {
+  return (
+    <StyleTagContainer displayContext={displayContext}>
+      <StyledTag>{children}</StyledTag>
+    </StyleTagContainer>
+  );
 }
