0474489a9ae9637f8f7a382e053189cd53048b77
ost-ptk <ostap@make.services>
2023-03-30 11:20:16 +0300

    removed dependency on the account balance to the currency rate

--- src/apps/popup/pages/home/index.tsx
+++ src/apps/popup/pages/home/index.tsx
@@ -129,15 +129,16 @@ export function HomePageContent() {
   useEffect(() => {
     dispatchFetchActiveAccountBalance(activeAccount?.publicKey)
       .then(({ payload: { balance, currencyRate } }) => {
-        if (balance != null && currencyRate != null) {
+        if (balance != null) {
           const amount = formatNumber(motesToCSPR(balance), {
             precision: { max: 5 }
           });
-          const fiatAmount = formatCurrency(
-            motesToCurrency(balance, currencyRate),
-            'USD',
-            { precision: 2 }
-          );
+          const fiatAmount =
+            currencyRate != null
+              ? formatCurrency(motesToCurrency(balance, currencyRate), 'USD', {
+                  precision: 2
+                })
+              : '-';
 
           setBalance({ amount, fiatAmount });
         }
