163355d2b2341c6c9fef83e987c006307437fb9e
Piotr Witek <piotrek.witek@gmail.com>
2023-03-31 15:46:30 +0200

    Added file resource cleanup

--- src/apps/popup/pages/download-secret-keys/utils.ts
+++ src/apps/popup/pages/download-secret-keys/utils.ts
@@ -1,9 +1,13 @@
 export function downloadFile(content: Blob, filename: string): void {
+  // allocation
   const url = window.URL.createObjectURL(content);
-  const downloadFileLink = document.createElement('a');
 
+  const downloadFileLink = document.createElement('a');
   downloadFileLink.href = url;
   downloadFileLink.setAttribute('download', filename);
   downloadFileLink.click();
   downloadFileLink.remove();
+
+  // cleanup
+  window.URL.revokeObjectURL(url);
 }
