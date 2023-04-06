bb47f4083c8c574bedbb11dd57cb9c09e16ab7f0
Piotr Witek <piotrek.witek@gmail.com>
2023-04-06 18:56:34 +0200

    Updated Safari scripts

--- scripts/create-xcode-project.sh
+++ scripts/create-xcode-project.sh
@@ -1,3 +1,4 @@
 # Converting Firefox build folder into a Xcode Project
 # Firefox extension is built on manifest V2, the same version Safari required
-xcrun safari-web-extension-converter --no-prompt --no-open --project-location ./.xcode-project --macos-only ./build/firefox
+rm -rf .xcode-project
+xcrun safari-web-extension-converter ./build/firefox --project-location ./.xcode-project --bundle-identifier software.make.Casper-Wallet --macos-only --no-open --no-prompt
