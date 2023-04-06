1e5579ff9d8d0a4485c174becc1ddff5625dffa7
ost-ptk <ostap@make.services>
2023-04-06 17:22:07 +0300

    updated test

--- e2e/tests/popup/connect-account/connect-test.test.ts
+++ e2e/tests/popup/connect-account/connect-test.test.ts
@@ -5,7 +5,7 @@ import { Driver } from '../../../webdriver/driver';
 import { buildWebDriver } from '../../../webdriver';
 import { AppRoutes } from '../../../app-routes';
 import { lockVault, unlockVault } from '../../common';
-import { ACCOUNT_NAMES, TIMEOUT } from '../../../constants';
+import { TIMEOUT } from '../../../constants';
 import { byText } from '../../../utils/helpers';
 
 describe('Test', () => {
@@ -15,50 +15,16 @@ describe('Test', () => {
     driver = await buildWebDriver();
 
     await driver.navigate(AppRoutes.Popup);
+
+    await unlockVault(driver);
+    await lockVault(driver);
   });
 
   afterAll(async () => {
     await driver.quit();
   });
 
-  it('should unlock vault', async () => {
-    await driver.wait(
-      until.elementLocated(byText('Your wallet is locked')),
-      TIMEOUT
-    );
-
-    await unlockVault(driver);
-
-    assert.ok(
-      await driver
-        .wait(
-          until.elementLocated(byText(ACCOUNT_NAMES.defaultAccountName)),
-          1000 * 60
-        )
-        .catch(async () => {
-          await driver.verboseReportOnFailure(`Failed on - account name`);
-        })
-    );
-  });
-
-  it('should lock vault', async () => {
-    await driver.wait(
-      until.elementLocated(byText(ACCOUNT_NAMES.defaultAccountName)),
-      TIMEOUT
-    );
-
-    await lockVault(driver);
-
-    assert.ok(
-      await driver
-        .wait(until.elementLocated(byText('Your wallet is locked')), 1000 * 60)
-        .catch(async () => {
-          await driver.verboseReportOnFailure(`Failed on - lock vault`);
-        })
-    );
-  });
-
-  it('should unlock vault 2 time', async () => {
+  it('should unlock vault 1', async () => {
     await driver.wait(
       until.elementLocated(byText('Your wallet is locked')),
       TIMEOUT
@@ -70,9 +36,38 @@ describe('Test', () => {
       await driver
         .wait(until.elementLocated(byText('Lock wallet')), 1000 * 60)
         .catch(async () => {
-          await driver.verboseReportOnFailure(
-            `Failed on - account name (unlock)`
-          );
+          await driver.verboseReportOnFailure(`Failed on - unlock vault 1`);
+        })
+    );
+  });
+
+  it('should lock vault 1', async () => {
+    await driver.wait(until.elementLocated(byText('Lock wallet')), TIMEOUT);
+
+    await driver.clickElement(byText('Lock wallet'));
+
+    assert.ok(
+      await driver
+        .wait(until.elementLocated(byText('Your wallet is locked')), 1000 * 60)
+        .catch(async () => {
+          await driver.verboseReportOnFailure(`Failed on - lock vault 1`);
+        })
+    );
+  });
+
+  it('should unlock vault 2', async () => {
+    await driver.wait(
+      until.elementLocated(byText('Your wallet is locked')),
+      TIMEOUT
+    );
+
+    await unlockVault(driver);
+
+    assert.ok(
+      await driver
+        .wait(until.elementLocated(byText('Lock wallet')), 1000 * 60)
+        .catch(async () => {
+          await driver.verboseReportOnFailure(`Failed on - unlock vault 2`);
         })
     );
   });
