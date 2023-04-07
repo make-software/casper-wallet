ef2e322faaa6fd11fa99ba83da565c1d7b9fb4f8
ost-ptk <ostap@make.services>
2023-04-07 11:05:05 +0300

    updated test v3

--- e2e/tests/popup/connect-account/connect-test.test.ts
+++ e2e/tests/popup/connect-account/connect-test.test.ts
@@ -16,12 +16,12 @@ describe('Test', () => {
 
     await driver.navigate(AppRoutes.Popup);
 
-    await unlockVault(driver).finally(async () => {
-      await driver.verboseReportOnFailure(`Unlock vault - 0`);
-    });
-    await lockVault(driver).finally(async () => {
-      await driver.verboseReportOnFailure(`Lock vault - 0`);
-    });
+    // await unlockVault(driver).finally(async () => {
+    //   await driver.verboseReportOnFailure(`Unlock vault - 0`);
+    // });
+    // await lockVault(driver).finally(async () => {
+    //   await driver.verboseReportOnFailure(`Lock vault - 0`);
+    // });
   });
 
   afterAll(async () => {
@@ -40,7 +40,7 @@ describe('Test', () => {
 
     assert.ok(
       await driver
-        .wait(until.elementLocated(byText('Lock wallet')), 1000 * 60)
+        .wait(until.elementLocated(byText('Account 1')), 1000 * 60)
         .catch(async () => {
           await driver.verboseReportOnFailure(`Failed on - unlock vault 1`);
         })
@@ -50,7 +50,7 @@ describe('Test', () => {
   it('should lock vault 1', async () => {
     // await driver.wait(until.elementLocated(byText('Lock wallet')), TIMEOUT);
 
-    await driver.clickElement(byText('Lock wallet')).finally(async () => {
+    await lockVault(driver).finally(async () => {
       await driver.verboseReportOnFailure(`Lock vault - 1`);
     });
 
