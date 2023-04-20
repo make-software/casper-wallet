29475f54db9de1b617cbadc466bdd32bf05f3f0d
Piotr Witek <piotrek.witek@gmail.com>
2023-04-20 10:35:05 +0200

    Added xcode project with App Store release configuration

---
+++ xcode-project/Casper Wallet/Casper Wallet/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Casper Wallet
+//
+//  Created by Piotrek Witek on 19/04/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
