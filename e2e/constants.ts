8fa2bc120798a8f4dd3321d5bf625ac53e2c2700
Dmytro Vynnyk <simbiatoff@gmail.com>
2023-07-21 00:05:56 +0300

    Fix e2e publicKey tests

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -9,8 +9,8 @@ export const PLAYGROUND_URL = 'https://casper-wallet-playground.make.services/';
 export const IMPORTED_ACCOUNT = {
   accountName: ACCOUNT_NAMES.importedAccountName,
   publicKey:
-    '0184f6d260F4EE6869DDB36affe15456dE6aE045278FA2f467bb677561cE0daD55',
-  truncatedPublicKey: '0184f...daD55'
+    '0184f6d260f4ee6869ddb36affe15456de6ae045278fa2f467bb677561ce0dad55',
+  truncatedPublicKey: '0184f...dad55'
 };
 
 export const DEFAULT_ACCOUNT = {
