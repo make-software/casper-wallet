d883c4a85c94c6d415f273c6aaeb52524670e117
ost-ptk <ostap@make.services>
2023-07-21 16:31:48 +0300

    moved cspr link button to the navigation bar

--- src/libs/layout/header/header-view-in-explorer.tsx
+++ src/libs/layout/header/header-view-in-explorer.tsx
@@ -5,57 +5,81 @@ import { Link, SvgIcon, Typography } from '@libs/ui';
 import { AlignedFlexRow, CenteredFlexColumn, SpacingSize } from '@libs/layout';
 import {
   getBlockExplorerAccountUrl,
-  getBlockExplorerContractUrl
+  getBlockExplorerContractUrl,
+  getBlockExplorerDeployUrl
 } from '@src/constants';
 import { formatErc20TokenBalance } from '@popup/pages/home/components/tokens-list/utils';
-import { useCasperToken } from '@src/hooks';
+import { useCasperToken, useErc20Tokens } from '@src/hooks';
 import { selectApiConfigBasedOnActiveNetwork } from '@background/redux/settings/selectors';
 import { selectVaultActiveAccount } from '@background/redux/vault/selectors';
-import { ContractPackageWithBalance } from '@libs/services/erc20-service';
 
 interface HeaderViewInExplorerProps {
-  erc20Tokens: ContractPackageWithBalance[] | null;
   tokenName?: string;
+  deployHash?: string;
 }
 
 export function HeaderViewInExplorer({
   tokenName,
-  erc20Tokens
+  deployHash
 }: HeaderViewInExplorerProps) {
-  const casperToken = useCasperToken();
   const [hrefToTokenOnCasperLive, setHrefToTokenOnCasperLive] = useState<
+    string | undefined
+  >();
+  const [hrefToDeployOnCasperLive, setHrefToDeployOnCasperLive] = useState<
     string | undefined
   >();
 
   const { casperLiveUrl } = useSelector(selectApiConfigBasedOnActiveNetwork);
   const activeAccount = useSelector(selectVaultActiveAccount);
 
-  useEffect(() => {
-    if (tokenName === 'Casper') {
-      if (casperToken && activeAccount) {
-        setHrefToTokenOnCasperLive(
-          getBlockExplorerAccountUrl(casperLiveUrl, activeAccount.publicKey)
-        );
-      }
-    } else {
-      // ERC-20 token case
-      const erc20TokensList = formatErc20TokenBalance(erc20Tokens);
-      const token = erc20TokensList?.find(token => token.id === tokenName);
+  const casperToken = useCasperToken();
+  const erc20Tokens = useErc20Tokens();
 
-      if (token) {
-        setHrefToTokenOnCasperLive(
-          getBlockExplorerContractUrl(casperLiveUrl, token.id)
-        );
+  useEffect(() => {
+    if (!tokenName && deployHash) {
+      setHrefToDeployOnCasperLive(
+        getBlockExplorerDeployUrl(casperLiveUrl, deployHash)
+      );
+    }
+
+    if (tokenName) {
+      if (tokenName === 'Casper') {
+        if (casperToken && activeAccount) {
+          setHrefToTokenOnCasperLive(
+            getBlockExplorerAccountUrl(casperLiveUrl, activeAccount.publicKey)
+          );
+        }
+      } else {
+        // ERC-20 token case
+        const erc20TokensList = formatErc20TokenBalance(erc20Tokens);
+        const token = erc20TokensList?.find(token => token.id === tokenName);
+
+        if (token) {
+          setHrefToTokenOnCasperLive(
+            getBlockExplorerContractUrl(casperLiveUrl, token.id)
+          );
+        }
       }
     }
-  }, [tokenName, casperToken, activeAccount, casperLiveUrl, erc20Tokens]);
+  }, [
+    tokenName,
+    casperToken,
+    activeAccount,
+    casperLiveUrl,
+    erc20Tokens,
+    deployHash
+  ]);
 
-  if (!hrefToTokenOnCasperLive) {
+  if (!hrefToTokenOnCasperLive && !hrefToDeployOnCasperLive) {
     return null;
   }
 
   return (
-    <Link color="inherit" target="_blank" href={hrefToTokenOnCasperLive}>
+    <Link
+      color="inherit"
+      target="_blank"
+      href={hrefToTokenOnCasperLive || hrefToDeployOnCasperLive}
+    >
       <CenteredFlexColumn gap={SpacingSize.Medium}>
         <AlignedFlexRow gap={SpacingSize.Small}>
           <Typography type="bodySemiBold" color="contentBlue">
