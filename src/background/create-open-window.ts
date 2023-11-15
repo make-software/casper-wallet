6132fb6449cbe0d59b705ffc3080f9ba1d51bdc5
ost-ptk <ostap@make.services>
2023-11-15 17:51:56 +0200

    testing

--- src/background/create-open-window.ts
+++ src/background/create-open-window.ts
@@ -142,6 +142,7 @@ export function createOpenWindow({
           }
           return newWindow;
         });
+        console.log(currentWindow.state);
 
         try {
           console.log(await window, 'window');
