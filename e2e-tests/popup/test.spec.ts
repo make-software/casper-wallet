b3a6f31850130246714b92d185a3cbddddf74979
ost-ptk <ostap@make.services>
2023-11-15 15:04:30 +0200

    testing

--- e2e-tests/popup/test.spec.ts
+++ e2e-tests/popup/test.spec.ts
@@ -2,6 +2,18 @@ import { test } from '../fixtures';
 import { vaultPassword } from '../common';
 
 test('test', async ({ extensionId, context, page }) => {
+  const importAccountPage = await context.newPage();
+  await importAccountPage.goto(
+    `chrome-extension://${extensionId}/import-account-with-file.html`
+  );
+
+  await test
+    .expect(
+      importAccountPage.getByRole('heading', {
+        name: 'Import account from secret key file'
+      })
+    )
+    .toBeVisible();
   await page.goto(`chrome-extension://${extensionId}/popup.html`);
 
   await page.getByPlaceholder('Password', { exact: true }).fill(vaultPassword);
@@ -9,14 +21,14 @@ test('test', async ({ extensionId, context, page }) => {
 
   await page.getByTestId('menu-open-icon').click();
 
-  const [importAccountPage] = await Promise.all([
+  const [importAccountPage2] = await Promise.all([
     context.waitForEvent('page'),
     await page.getByText('Import account').click()
   ]);
 
   await test
     .expect(
-      importAccountPage.getByRole('heading', {
+      importAccountPage2.getByRole('heading', {
         name: 'Import account from secret key file'
       })
     )
