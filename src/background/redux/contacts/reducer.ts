193451605af56a0ef48e1648ad79a73b907ae286
ost-ptk <ostap@make.services>
2023-12-06 11:26:09 +0200

    added contact sort and fixed the issue with updating contact

--- src/background/redux/contacts/reducer.ts
+++ src/background/redux/contacts/reducer.ts
@@ -11,8 +11,11 @@ const initialState = { contacts: [], lastModified: null } as ContactsState;
 
 export const reducer = createReducer(initialState)
   .handleAction(newContactAdded, (state, action) => {
+    const sortedContacts = [...state.contacts, action.payload].sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
     return {
-      contacts: [...state.contacts, action.payload],
+      contacts: sortedContacts,
       lastModified: action.payload.lastModified
     };
   })
@@ -25,9 +28,14 @@ export const reducer = createReducer(initialState)
     };
   })
   .handleAction(contactUpdated, (state, { payload }) => {
+    const newContacts = {
+      name: payload.name,
+      publicKey: payload.publicKey,
+      lastModified: payload.lastModified
+    };
     return {
       contacts: state.contacts.map(contact =>
-        contact.name === payload.oldName ? payload : contact
+        contact.name === payload.oldName ? newContacts : contact
       ),
       lastModified: payload.lastModified
     };
