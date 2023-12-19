801d311d2a4f6071b574a3539381bf9fed1159d7
Dmytro Vynnyk <simbiatoff@gmail.com>
2023-12-19 20:13:40 +0200

    Configure build_all.sh

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,3 +1,3 @@
 HASH=$(git rev-parse --short HEAD)
 
-npm run build:chrome && npm run build:firefox && cd ./build && zip -r casper-wallet#$HASH.zip && npm run build:src
+npm run build:chrome && npm run build:firefox && cd ./build && zip -r casper-wallet#$HASH.zip ./* && npm run build:src
