d4c9930305443e0fd00295255b6b60d9e4d10463
Vynnyk Dmytro <simbiatoff@gmail.com>
2024-03-07 11:48:55 +0200

    Release/1.8.3 (#952)
    * Add support for JPG format in token icons
    
    * Add validation for transaction fee field
    
    * Update UI to handle large balance and name in account plate and fix id for token activity list
    
    * Fix issue with big numbers
    
    * update UI to handle large value amount in activity details page and activity plate
    
    * Release 1.8.3 version
    
    ---------
    
    Co-authored-by: ost-ptk <gestap9@gmail.com>

--- src/libs/ui/components/token-plate/token-plate.tsx
+++ src/libs/ui/components/token-plate/token-plate.tsx
@@ -28,6 +28,11 @@ const ListItemContainer = styled(AlignedSpaceBetweenFlexRow)<{
   padding: ${({ chevron }) => (chevron ? '10px 12px 10px 16px' : '10px 16px')};
 `;
 
+const LogoImg = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
 interface TokenPlateProps {
   token: TokenType | null;
   chevron?: boolean;
@@ -38,65 +43,80 @@ export const TokenPlate = ({
   token,
   chevron,
   handleOnClick
-}: TokenPlateProps) => (
-  <ListItemContainer
-    chevron={chevron}
-    gap={SpacingSize.Small}
-    onClick={handleOnClick}
-    clickable={!!handleOnClick}
-  >
-    <AlignedFlexRow gap={SpacingSize.Medium}>
-      <SvgIcon src={token?.icon || ''} size={32} />
-      <TokenNameContainer>
-        <Tooltip
-          title={token?.name && token.name.length > 10 ? token.name : undefined}
-          fullWidth
-          overflowWrap
-        >
-          <Typography type="body" ellipsis loading={!token?.name}>
-            {token?.name}
-          </Typography>
-        </Tooltip>
-      </TokenNameContainer>
-    </AlignedFlexRow>
-    <AlignedFlexRow gap={SpacingSize.Small}>
-      <TokenAmountContainer>
-        <Tooltip
-          title={
-            (token?.amount && token.amount.length > 7) ||
-            (token?.symbol && token.symbol.length > 6)
-              ? `${token?.amount} ${token?.symbol}`
-              : undefined
-          }
-          placement="bottomLeft"
-          overflowWrap
-          fullWidth
-        >
-          <RightAlignedCenteredFlexRow gap={SpacingSize.Small}>
-            <Typography type="bodyHash" ellipsis loading={!token?.amount}>
-              {token?.amount}
-            </Typography>
-            <Typography
-              type="bodyHash"
-              color="contentSecondary"
-              ellipsis={!!(token?.symbol && token.symbol.length > 6)}
-              loading={!token?.symbol && token?.symbol !== ''}
-            >
-              {token?.symbol}
-            </Typography>
-          </RightAlignedCenteredFlexRow>
-        </Tooltip>
-        {token?.name === 'Casper' && (
-          <Typography
-            type="listSubtext"
-            color="contentSecondary"
-            loading={!token?.amountFiat}
-          >
-            {token?.amountFiat}
-          </Typography>
+}: TokenPlateProps) => {
+  const tokenIconFormat = token?.icon?.split('.').pop();
+  const isTokenIconJPG = tokenIconFormat === 'jpg';
+
+  return (
+    <ListItemContainer
+      chevron={chevron}
+      gap={SpacingSize.Small}
+      onClick={handleOnClick}
+      clickable={!!handleOnClick}
+    >
+      <AlignedFlexRow gap={SpacingSize.Medium}>
+        {isTokenIconJPG ? (
+          <LogoImg
+            src={token?.icon || ''}
+            alt={token?.name}
+            title={token?.name}
+          />
+        ) : (
+          <SvgIcon src={token?.icon || ''} alt={token?.name} size={32} />
         )}
-      </TokenAmountContainer>
-      {chevron && <SvgIcon src="assets/icons/chevron.svg" size={16} />}
-    </AlignedFlexRow>
-  </ListItemContainer>
-);
+        <TokenNameContainer>
+          <Tooltip
+            title={
+              token?.name && token.name.length > 10 ? token.name : undefined
+            }
+            fullWidth
+            overflowWrap
+          >
+            <Typography type="body" ellipsis loading={!token?.name}>
+              {token?.name}
+            </Typography>
+          </Tooltip>
+        </TokenNameContainer>
+      </AlignedFlexRow>
+      <AlignedFlexRow gap={SpacingSize.Small}>
+        <TokenAmountContainer>
+          <Tooltip
+            title={
+              (token?.amount && token.amount.length > 7) ||
+              (token?.symbol && token.symbol.length > 6)
+                ? `${token?.amount} ${token?.symbol}`
+                : undefined
+            }
+            placement="bottomLeft"
+            overflowWrap
+            fullWidth
+          >
+            <RightAlignedCenteredFlexRow gap={SpacingSize.Small}>
+              <Typography type="bodyHash" ellipsis loading={!token?.amount}>
+                {token?.amount}
+              </Typography>
+              <Typography
+                type="bodyHash"
+                color="contentSecondary"
+                ellipsis={!!(token?.symbol && token.symbol.length > 6)}
+                loading={!token?.symbol && token?.symbol !== ''}
+              >
+                {token?.symbol}
+              </Typography>
+            </RightAlignedCenteredFlexRow>
+          </Tooltip>
+          {token?.name === 'Casper' && (
+            <Typography
+              type="listSubtext"
+              color="contentSecondary"
+              loading={!token?.amountFiat}
+            >
+              {token?.amountFiat}
+            </Typography>
+          )}
+        </TokenAmountContainer>
+        {chevron && <SvgIcon src="assets/icons/chevron.svg" size={16} />}
+      </AlignedFlexRow>
+    </ListItemContainer>
+  );
+};
