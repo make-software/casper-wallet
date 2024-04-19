cc84cab3b3d740711fae8b74a4cb51429791f8c4
ost-ptk <gestap9@gmail.com>
2024-04-19 14:22:37 +0300

    fix issue with infinity connection to WebSocket for devtools

--- src/background/redux/index.ts
+++ src/background/redux/index.ts
@@ -1,3 +1,4 @@
+import { composeWithDevTools } from '@redux-devtools/remote';
 import {
   applyMiddleware, // TODO: Move to actual `createStore`
   compose,
@@ -6,30 +7,30 @@ import {
 import createSagaMiddleware from 'redux-saga';
 import { RootState } from 'typesafe-actions';
 
+import { isChromeBuild } from '@src/utils';
+
 import reduxAction from './redux-action';
 import rootReducer from './root-reducer';
 import rootSaga from './root-saga';
 
-// Disabled redux devtools
-// until we find a solution to fix the issue with an infinite number of times to connect to the WebSocket server
-
-// import { composeWithDevTools } from '@redux-devtools/remote';
-// export const composeEnhancers =
-//   process.env.NODE_ENV === 'development' && isChromeBuild
-//     ? composeWithDevTools({
-//         name: 'Casper Wallet',
-//         hostname: 'localhost',
-//         port: 8000
-//       })
-//     : compose;
+export const composeEnhancers =
+  process.env.NODE_ENV === 'development' && isChromeBuild
+    ? composeWithDevTools({
+        name: 'Casper Wallet',
+        hostname: 'localhost',
+        port: 8000
+      })
+    : compose;
 
 export const createStore = (initialState: Partial<RootState>) => {
   const sagaMiddleware = createSagaMiddleware();
   // configure middlewares
   const middlewares = [sagaMiddleware];
   // compose enhancers
-  const enhancer = compose(applyMiddleware(...middlewares));
+  // @ts-ignore
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
   // create store
+  // @ts-ignore
   const store = createStoreRedux(rootReducer, initialState, enhancer);
   // run sagas
   sagaMiddleware.run(rootSaga);
