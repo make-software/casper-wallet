9a1daa482065f1bdbbdb1b766234c1a2c916f5c4
Ostap Piatkovskyi <44294945+ost-ptk@users.noreply.github.com>
2025-01-18 17:59:12 +0200

    fix: refactor popover implementation with `react-tiny-popover`. (#1113)
    * Refactor popover implementation with `react-tiny-popover`.
    
    * Add test ID to account popover icon for improved testing

--- src/libs/ui/components/account-popover/account-popover.tsx
+++ src/libs/ui/components/account-popover/account-popover.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 import { getBlockExplorerAccountUrl } from '@src/constants';
 
@@ -16,20 +17,33 @@ import {
   selectIsAnyAccountConnectedWithActiveOrigin
 } from '@background/redux/vault/selectors';
 
+import { FlexColumn } from '@libs/layout';
 import { Account } from '@libs/types/account';
 import { PopoverLink, SvgIcon, Typography } from '@libs/ui/components';
 import { Popover } from '@libs/ui/components/popover/popover';
+
+const PopoverItemsContainer = styled(FlexColumn)`
+  padding: 8px;
+
+  background: ${({ theme }) => theme.color.backgroundPrimary};
+  box-shadow: ${({ theme }) => theme.shadow.contextMenu};
+  border-radius: ${({ theme }) => theme.borderRadius.eight}px;
+`;
 
 interface AccountActionsMenuPopoverProps {
   account: Account;
   onClick?: (e: React.MouseEvent) => void;
   showHideAccountItem?: boolean;
+  popoverParentRef: React.MutableRefObject<HTMLDivElement | null>;
 }
 export const AccountActionsMenuPopover = ({
   account,
   onClick,
-  showHideAccountItem
+  showHideAccountItem,
+  popoverParentRef
 }: AccountActionsMenuPopoverProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const navigate = useTypedNavigate();
   const { t } = useTranslation();
   const { disconnectAccountWithEvent: disconnectAccount } = useAccountManager();
@@ -44,13 +58,16 @@ export const AccountActionsMenuPopover = ({
 
   return (
     <Popover
-      renderMenuItems={({ closePopover }) => (
-        <>
+      popoverParentRef={popoverParentRef}
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      content={() => (
+        <PopoverItemsContainer>
           {connectedAccountNames.includes(account.name) ? (
             <PopoverLink
               variant="contentAction"
               onClick={event => {
-                closePopover(event);
+                setIsOpen(false);
                 activeOrigin && disconnectAccount(account.name, activeOrigin);
 
                 if (onClick) {
@@ -176,10 +193,14 @@ export const AccountActionsMenuPopover = ({
               <Trans t={t}>Manage</Trans>
             </Typography>
           </PopoverLink>
-        </>
+        </PopoverItemsContainer>
       )}
     >
-      <SvgIcon src="assets/icons/more.svg" />
+      <SvgIcon
+        src="assets/icons/more.svg"
+        data-testid="popover-children-container"
+        onClick={() => setIsOpen(!isOpen)}
+      />
     </Popover>
   );
 };
