56a20ba96b6e853f696dfda3b3d95587703ab821
Dmytro Vynnyk <simbiatoff@gmail.com>
2026-03-30 14:30:27 +0200

    Release 2.4.0 version

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,3 +1,3 @@
 HASH=$(git rev-parse --short HEAD)
 
-npm run build:chrome && npm run build:firefox && cd ./build && zip -r casper-wallet-2.4.0rc5#$HASH.zip ./* && npm run build:src
+npm run build:chrome && npm run build:firefox && cd ./build && zip -r casper-wallet-2.4.0rc7#$HASH.zip ./* && npm run build:src
