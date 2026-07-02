ef28aac47baf04a853187ef9c9e037bb4c9a11ef
ost-ptk <ostap@make.services>
2026-07-02 09:40:14 +0300

    Revert "build(deps): DEP-7 — redux 5 + react-redux 9 + reselect 5 + redux-saga 1.5 (WALLET-1333)"
    This reverts commit 866c822383dcd0f18c057b50e81826bd1a810fd8.

--- src/background/redux/contacts/selectors.ts
+++ src/background/redux/contacts/selectors.ts
@@ -1,4 +1,3 @@
-import { createSelector } from 'reselect';
 import { RootState } from 'typesafe-actions';
 
 export const selectAllContacts = (state: RootState) => state.contacts.contacts;
@@ -6,15 +5,11 @@ export const selectAllContacts = (state: RootState) => state.contacts.contacts;
 export const selectCountOfContacts = (state: RootState) =>
   state.contacts.contacts.length;
 
-export const selectAllContactsNames = createSelector(
-  selectAllContacts,
-  contacts => contacts.map(contact => contact.name)
-);
+export const selectAllContactsNames = (state: RootState) =>
+  state.contacts.contacts.map(contact => contact.name);
 
-export const selectAllContactsPublicKeys = createSelector(
-  selectAllContacts,
-  contacts => contacts.map(contact => contact.publicKey)
-);
+export const selectAllContactsPublicKeys = (state: RootState) =>
+  state.contacts.contacts.map(contact => contact.publicKey);
 
 export const selectLastModified = (state: RootState) =>
   state.contacts.lastModified;
--- src/background/redux/vault/selectors.ts
+++ src/background/redux/vault/selectors.ts
@@ -19,15 +19,12 @@ export const selectSecretPhrase = (state: RootState): null | SecretPhrase =>
 export const selectVaultAccounts = (state: RootState): Account[] =>
   state.vault.accounts;
 
-export const selectVaultAccountsPublicKeys = createSelector(
-  selectVaultAccounts,
-  accounts => accounts.map(account => account.publicKey)
-);
+export const selectVaultAccountsPublicKeys = (state: RootState): string[] =>
+  state.vault.accounts.map(key => key.publicKey);
 
-export const selectVaultAccountsExceptLedgersAccounts = createSelector(
-  selectVaultAccounts,
-  accounts => accounts.filter(account => !account.hardware)
-);
+export const selectVaultAccountsExceptLedgersAccounts = (
+  state: RootState
+): Account[] => state.vault.accounts.filter(account => !account.hardware);
 
 export const selectVaultCountsOfAccounts = createSelector(
   selectVaultAccounts,
@@ -149,13 +146,12 @@ export const selectAccountsByOriginDict = createSelector(
 
 export const selectIsAccountConnected = createSelector(
   selectAccountNamesByOriginDict,
-  (_: RootState, origin: string | undefined) => origin,
   (
     _: RootState,
-    _origin: string | undefined,
+    origin: string | undefined,
     accountName: string | undefined
-  ) => accountName,
-  (accountNamesByOriginDict, origin, accountName) => {
+  ) => [origin, accountName],
+  (accountNamesByOriginDict, [origin, accountName]) => {
     const accountNames: '' | string[] | undefined =
       origin && accountNamesByOriginDict[origin];
     if (accountNames == null || !accountName) {
